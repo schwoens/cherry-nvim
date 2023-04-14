5eb1b26050396eea9d593f4db7cb22f89a15d9e0
schwoens <schwoens@protonmail.com>
2023-04-14 02:03:18 +0200

    initial commit

---
+++ colors/cherry.vim
@@ -0,0 +1,4 @@
+lua << EOF
+local cherry = require("cherry")
+cherry.setup({})
+EOF
---
+++ lua/cherry/palette.lua
@@ -0,0 +1,16 @@
+local colors = {
+  fg = "#DADADA",
+  bg = "#121416",
+  white = "#DADADA",
+  gray = "#B0B5B3",
+  blue = "#EDF2FC",
+  green = "#EDFDF3",
+  cyan = "#EDFBFC",
+  red = "#F7CACE",
+  orange = "#FFC999",
+  yellow = "#FCDDBC",
+  purple = "#BA7BA1",
+  magenta = "#B24C63",
+}
+
+return colors
